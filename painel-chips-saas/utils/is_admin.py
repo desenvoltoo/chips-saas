6de75a5a712757e7248a545866cd07dceb85b4d4
desenvoltoo <matheuscosta.tec@icloud.com>
2025-12-01 12:59:30 -0300

    Update is_admin.py

--- painel-chips-saas/utils/is_admin.py
+++ painel-chips-saas/utils/is_admin.py
@@ -1,10 +1,32 @@
-from functools import wraps
 from flask import session, redirect
 
-def admin_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if session.get("is_admin") != True:
-            return redirect("/dashboard")
+def login_required(f):
+    def wrapper(*args, **kwargs):
+        if "id_usuario" not in session:
+            return redirect("/")
         return f(*args, **kwargs)
-    return wrap
+    return wrapper
+
+
+def admin_only(f):
+    def wrapper(*args, **kwargs):
+        if session.get("tipo_usuario") not in ("admin", "superadmin"):
+            return "Acesso negado (admin)", 403
+        return f(*args, **kwargs)
+    return wrapper
+
+
+def superadmin_only(f):
+    def wrapper(*args, **kwargs):
+        if session.get("tipo_usuario") != "superadmin":
+            return "Acesso negado (superadmin)", 403
+        return f(*args, **kwargs)
+    return wrapper
+
+
+def empresa_only(f):
+    def wrapper(*args, **kwargs):
+        if "id_empresa" not in session:
+            return "Acesso negado (empresa)", 403
+        return f(*args, **kwargs)
+    return wrapper
