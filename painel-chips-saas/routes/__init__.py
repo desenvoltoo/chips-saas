c5357293065ccfbdaac043df772aea9cfc65672e
desenvoltoo <matheuscosta.tec@icloud.com>
2025-12-01 13:18:54 -0300

    Update __init__.py

--- painel-chips-saas/routes/__init__.py
+++ painel-chips-saas/routes/__init__.py
@@ -1,17 +1,18 @@
-from .auth import auth_bp
-from .dashboard import bp_dashboard
 from .chips import chips_bp
 from .aparelhos import aparelhos_bp
 from .recargas import recargas_bp
 from .relacionamentos import relacionamentos_bp
+from .dashboard import dashboard_bp
 from .admin import admin_bp
-
+from .admin_empresa import admin_empresa_bp
+from .auth import auth_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
-    app.register_blueprint(bp_dashboard)
     app.register_blueprint(chips_bp)
     app.register_blueprint(aparelhos_bp)
     app.register_blueprint(recargas_bp)
     app.register_blueprint(relacionamentos_bp)
+    app.register_blueprint(dashboard_bp)
     app.register_blueprint(admin_bp)
+    app.register_blueprint(admin_empresa_bp)
